3c2b073edf5bd99dab8a2a793b007d3d5b8071a4
sergeytompson <gfhfahfp@gmail.com>
2023-05-28 20:03:16 +0300

    fix: drop standard comments, changed model parameters, corrected the settings according to the review.

---
+++ dev_bridge_admin/emails/models.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Template(models.Model):
+    name = models.CharField("название шаблона", max_length=150)
+    template = models.FileField(
+        "HTML шаблон письма", upload_to="emails/templates/", max_length=200
+    )
+
+    def __str__(self) -> str:
+        return self.name[:20]
+
+    class Meta:
+        verbose_name = "шаблон"
+        verbose_name_plural = "шаблоны"
+
+
+class Attachment(models.Model):
+    name = models.CharField("название вложения", max_length=150)
+    attachment = models.FileField(
+        "вложение", upload_to="emails/attachment/", max_length=200
+    )
+
+    def __str__(self) -> str:
+        return self.name[:20]
+
+    class Meta:
+        verbose_name = "вложение"
+        verbose_name_plural = "вложения"
+
+
+class EmailToSend(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name="автор",
+        related_name="email_to_send",
+    )
+    recipients = models.ManyToManyField(
+        User,
+        verbose_name="получатели",
+        related_name="recipient_emails_to_send",
+    )
+    template = models.ForeignKey(
+        Template,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="шаблон",
+    )
+    attachments = models.ManyToManyField(
+        Attachment,
+        verbose_name="вложения",
+    )
+    confirm_url = models.URLField(
+        "ссылка для подтверждения",
+        blank=True,
+    )
+    is_high_priority = models.BooleanField(
+        "высокий приоритет",
+        default=False,
+    )
+
+    def __str__(self) -> str:
+        return f"Письмо №{self.pk}"
+
+    class Meta:
+        verbose_name = "письмо для отправки"
+        verbose_name_plural = "письма для отправки"
