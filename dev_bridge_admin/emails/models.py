59ebbbe36f16ab398b724b25da151e16755b948b
sergeytompson <gfhfahfp@gmail.com>
2023-06-23 21:48:55 +0300

    feat: add SentEmail and NotSentEmail models. Add abstract BaseEmail model.

--- dev_bridge_admin/emails/models.py
+++ dev_bridge_admin/emails/models.py
@@ -2,6 +2,8 @@
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
+from .validators import MimeTypeValidator
 
 NAME_LIMIT = 20
 CHAR_FIELD_MAX_LENGTH = 150
@@ -13,7 +15,10 @@ class Template(models.Model):
 
     name = models.CharField("название шаблона", max_length=CHAR_FIELD_MAX_LENGTH)
     template = models.FileField(
-        "HTML шаблон письма", upload_to="emails/templates/", max_length=FILE_FIELD_MAX_LENGTH
+        "HTML шаблон письма",
+        upload_to="emails/templates/",
+        max_length=FILE_FIELD_MAX_LENGTH,
+        validators=[MimeTypeValidator({"html": "text/html"})],
     )
 
     class Meta:
@@ -33,6 +38,7 @@ class Attachment(models.Model):
     attachment = models.FileField(
         "вложение", upload_to="emails/attachment/", max_length=FILE_FIELD_MAX_LENGTH
     )
+    is_template = models.BooleanField("является шаблоном", default=False)
 
     class Meta:
         verbose_name = "вложение"
@@ -44,30 +50,28 @@ class Attachment(models.Model):
         return self.name[:NAME_LIMIT]
 
 
-class EmailToSend(models.Model):
-    """Модель письма, подготовленного для отправки"""
+class AbstractEmail(models.Model):
+    """Базовая модель письма"""
 
     author = models.ForeignKey(
         User,
         on_delete=models.PROTECT,
         verbose_name="автор",
-        related_name="email_to_send",
-    )
-    recipients = models.ManyToManyField(
-        User,
-        verbose_name="получатели",
-        related_name="recipient_emails_to_send",
+        related_name="%(class)s_set",
+        null=True,
+        blank=True,
     )
     template = models.ForeignKey(
         Template,
         on_delete=models.PROTECT,
         verbose_name="шаблон",
-        related_name="emails_to_send"
+        related_name="%(class)s_set",
     )
     attachments = models.ManyToManyField(
         Attachment,
         verbose_name="вложения",
-        related_name="emails_to_send"
+        related_name="%(class)s_set",
+        blank=True,
     )
     confirm_url = models.URLField(
         "ссылка для подтверждения",
@@ -77,11 +81,130 @@ class EmailToSend(models.Model):
         "высокий приоритет",
         default=False,
     )
+    is_email_to_confirm = models.BooleanField(
+        "письмо для подтверждения почты",
+        default=False,
+    )
+    is_email_to_anonym = models.BooleanField(
+        "письмо анонимным пользователям",
+        default=False,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class EmailToSend(AbstractEmail):
+    """Модель письма, подготовленного для отправки"""
+
+    recipients = models.ManyToManyField(
+        User,
+        verbose_name="получатели",
+        related_name="recipient_emails_to_send",
+        blank=True,
+    )
+    created = models.DateTimeField(
+        "создано",
+        auto_now_add=True,
+    )
 
     class Meta:
         verbose_name = "письмо для отправки"
         verbose_name_plural = "письма для отправки"
         ordering = ("is_high_priority",)
+        constraints = (
+            models.CheckConstraint(
+                check=Q(
+                    confirm_url__exact=""
+                ) & Q(
+                    is_email_to_confirm__exact=False
+                ) | Q(
+                    confirm_url__regex=r'.+'
+                ) & Q(
+                    is_email_to_confirm__exact=True
+                ),
+                name="url_for_email_to_confirm",
+                violation_error_message="Письма для подтверждения почты должны включать ссылку, "
+                                        "остальные письма - не должны.",
+            ),
+            models.CheckConstraint(
+                check=Q(
+                    author__isnull=True
+                ) & (
+                    Q(
+                        is_email_to_confirm__exact=True
+                    ) | Q(
+                        is_email_to_anonym=True
+                    )
+                ) | Q(
+                    author__isnull=False
+                ) & (
+                    Q(
+                        is_email_to_confirm__exact=False
+                    ) & Q(
+                        is_email_to_anonym=False
+                    )
+                ),
+                name="empty_author_for_email_to_confirm_and_to_anonym",
+                violation_error_message="В письмах с подтверждением почты и в письмах для анонимных "
+                                        "юзеров автор указывться не должен, в остальных письмах его "
+                                        "указывать обязательно.",
+            ),
+            models.CheckConstraint(
+                check=Q(
+                    is_email_to_anonym__exact=True
+                ) & Q(
+                    is_email_to_confirm__exact=False
+                ) | Q(
+                    is_email_to_anonym__exact=False
+                ) & Q(
+                    is_email_to_confirm__exact=True
+                ) | Q(
+                    is_email_to_anonym__exact=False
+                ) & Q(
+                    is_email_to_confirm__exact=False
+                ),
+                name="email_to_confirm_and_to_anonym_cant_be_true_together",
+                violation_error_message="Письмо для анонимных юзеров не может быть письмом для подтвеждения "
+                                        "почты (и наоборот).",
+            ),
+        )
 
     def __str__(self):
         return f"Письмо №{self.pk}"
+
+
+class SentEmail(AbstractEmail):
+    """Модель отправленного письма"""
+
+    created = models.DateTimeField("создано")
+    sent = models.DateTimeField("отправлено", auto_now_add=True)
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="получатель",
+        related_name="recipient_sent_emails",
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "отправленное письмо"
+        verbose_name_plural = "отправленные письма"
+
+
+class NotSentEmail(AbstractEmail):
+    """Модель не отправленного письма"""
+
+    created = models.DateTimeField("создано")
+    sending_attempt = models.DateTimeField("отправлено", auto_now_add=True)
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="получатель",
+        related_name="recipient_not_sent_emails",
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "неотправленное письмо"
+        verbose_name_plural = "неотправленные письма"
